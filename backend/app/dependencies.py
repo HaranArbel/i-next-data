00fa9ac448e0d255e7a6ff4dc28b8db724207bb2
Haran Arbel <haranarbel@gmail.com>
2025-04-24 19:02:37 +0300

    changed PatientService to injected dependency

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -2,6 +2,10 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from .db.database import get_db
 from .services.monitoring import MonitoringService, MonitoringServiceBase
+from .services.patient import PatientService, PatientServiceBase
 
 def get_monitoring_service(db: Session = Depends(get_db)) -> MonitoringServiceBase:
-    return MonitoringService(db) 
+    return MonitoringService(db)
+
+def get_patient_service(db: Session = Depends(get_db)) -> PatientServiceBase:
+    return PatientService(db) 
